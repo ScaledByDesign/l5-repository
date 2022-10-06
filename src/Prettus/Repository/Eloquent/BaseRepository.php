a2c8fad24a8ec53ca7e8e96e0ed877a2fc322227
Henry Fuentes <henry@scaledbydesign.com>
2022-10-06 17:10:27 -0600

    app reference

--- src/Prettus/Repository/Eloquent/BaseRepository.php
+++ src/Prettus/Repository/Eloquent/BaseRepository.php
@@ -634,7 +634,7 @@ abstract class BaseRepository implements RepositoryInterface, RepositoryCriteria
             // we should pass data that has been casts by the model
             // to make sure data type are same because validator may need to use
             // this data to compare with data that fetch from database.
-            if ($this->versionCompare($this->app->version(), "5.2.*", ">")) {
+            if ($this->versionCompare(app()->version(), "5.2.*", ">")) {
                 $attributes = $this->model->newInstance()->forceFill($attributes)->makeVisible($this->model->getHidden())->toArray();
             } else {
                 $model = $this->model->newInstance()->forceFill($attributes);
@@ -677,7 +677,7 @@ abstract class BaseRepository implements RepositoryInterface, RepositoryCriteria
             $model = $this->model->newInstance();
             $model->setRawAttributes([]);
             $model->setAppends([]);
-            if ($this->versionCompare($this->app->version(), "5.2.*", ">")) {
+            if ($this->versionCompare(app()->version(), "5.2.*", ">")) {
                 $attributes = $model->forceFill($attributes)->makeVisible($this->model->getHidden())->toArray();
             } else {
                 $model->forceFill($attributes);
